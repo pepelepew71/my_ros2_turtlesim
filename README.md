97a7f89fc46ef1ffeb8257665c133760fabe4a11
yc.huang71 <yc.huang71@gmail.com>
2023-02-25 02:10:02 +0800

    i

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ```
 ros2 launch my_ros2_turtlesim a.launch.py
-ros2 launch my_ros2_turtlesim b.launch.xml
 ```
 
 ![](./pics/demo.gif)
--- my_ros2_turtlesim/tracking.py
+++ my_ros2_turtlesim/tracking.py
@@ -13,18 +13,18 @@ from turtlesim.srv import Spawn, Kill
 
 class Target:
 
-    def __init__(self, master: "Tracking"):
+    def __init__(self, master: "Tracker"):
         """
-        Due to kill and spawn the new turtle with the same name can't be shown on
-        the turtlesim. So, use a serial number (num) to create new target turtle.
+        Kill and spawn a turtle with the same name is not working for turtlesim.
         """
-        self.master = master
-        self.num = 1
+        self._master = master
+        self._serial_number = 1
         self.x = None
         self.y = None
         self.t = None
+        self.spawn()  # spawn first target turtle
 
-    def set_random_state(self):
+    def _set_random_state(self):
         self.x = random.uniform(0.0, 10.0)
         self.y = random.uniform(0.0, 10.0)
         self.t = random.uniform(-math.pi, math.pi)
@@ -32,37 +32,36 @@ class Target:
     def get_distance_to(self, x: float, y: float):
         return ((self.x - x)**2.0 + (self.y - y)**2.0)**0.5
 
+    def kill(self):
+        req = Kill.Request()
+        req.name = "target"+str(self._serial_number)
+        self._master.srv_client_kill.call_async(request=req)
+        self._master.get_logger().info("kill target"+str(self._serial_number))
+
     def spawn(self):
-        self.set_random_state()
-        self.num += 1
+        self._set_random_state()
+        self._serial_number += 1
         req = Spawn.Request()
         req.x = self.x
         req.y = self.y
         req.theta = self.t
-        req.name = "target"+str(self.num)
-        self.master.srv_client_spawn.call_async(request=req)
-        self.master.get_logger().info(f'spawn target{self.num} at ({self.x:.3f}, {self.y:.3f})')
+        req.name = "target"+str(self._serial_number)
+        self._master.srv_client_spawn.call_async(request=req)
+        self._master.get_logger().info(f'spawn target{self._serial_number} at ({self.x:.3f}, {self.y:.3f})')
 
 
-class Tracking(Node):
+class Tracker(Node):
 
-    SPAWN_DISTANCE = 0.25
+    DISTANCE_TOL = 0.25
 
     def __init__(self):
         """
-        2 service clients to re-create target turtle. Subscribe turtle1's position,
-        using P-control to minimize the position between target and turtle1, then
-        publish command to turtle1.
-
-        Attributes:
-            srv_client_spawn
-            srv_client_kill
-            target
-            pub_turtle1_cmdvel
+        Use services /turtlesim/kill and /turtlesim/spawn to keep creating a
+        new turtle<number>. Then use P-control to control turtle1's location.
         """
         super().__init__(node_name="tracking")
-        self.get_logger().info(message="tracking start")
 
+        # -- service client
         self.srv_client_spawn = self.create_client(srv_type=Spawn, srv_name="/spawn")
         while not self.srv_client_spawn.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("Waiting for /spawn")
@@ -71,26 +70,26 @@ class Tracking(Node):
         while not self.srv_client_kill.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("Waiting for /kill")
 
-        self.target = Target(master=self)
-        self.target.spawn()
+        # -- subscriber
+        self.create_subscription(msg_type=Pose, topic="/turtle1/pose", callback=self._callback_turtle1_pose, qos_profile=10)
 
-        self.create_subscription(msg_type=Pose, topic="/turtle1/pose", callback=self._cb_turtle1_pose, qos_profile=10)
+        # -- publisher
         self.pub_turtle1_cmdvel = self.create_publisher(msg_type=Twist, topic="/turtle1/cmd_vel", qos_profile=10)
 
-    def _cb_turtle1_pose(self, msg):
+        # -- target object
+        self.target = Target(master=self)
+
+    def _callback_turtle1_pose(self, msg):
         """
         Callback for subscriber /turtle1/pose.
         """
         dist = self.target.get_distance_to(x=msg.x, y=msg.y)
-        if dist < __class__.SPAWN_DISTANCE:
-            req = Kill.Request()
-            req.name = "target"+str(self.target.num)
-            self.srv_client_kill.call_async(request=req)
-            self.get_logger().info("Kill target"+str(self.target.num))
+        if dist < __class__.DISTANCE_TOL:
+            self.target.kill()
             self.target.spawn()
         else:
             twist = Twist()
-            twist.linear.x = 1.0 if dist < 2.0 else dist / 2.0  # speed up
+            twist.linear.x = 1.0 if dist < 2.0 else dist / 2.0
             twist.angular.z = (math.atan2(self.target.y - msg.y, self.target.x - msg.x) - msg.theta)
             if twist.angular.z > math.pi:
                 twist.angular.z -= 2.0*math.pi
@@ -98,13 +97,14 @@ class Tracking(Node):
                 twist.angular.z += 2.0*math.pi
             else:
                 pass
-            twist.angular.z *= 2.0  # speed up
+            twist.angular.z *= 2.0
             self.pub_turtle1_cmdvel.publish(msg=twist)
 
 
 def main():
     rclpy.init(args=None)
-    node = Tracking()
+    node = Tracker()
+    node.get_logger().info(message="tracking start")
     rclpy.spin(node=node)
     rclpy.shutdown()
 
